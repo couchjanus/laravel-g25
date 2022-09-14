e8b04ae25e2b8aee1d5265911a6cdc34d1df31e6
couchjanus <couchjanus@gmail.com>
2022-09-14 20:55:13 +0300

    migration

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    // $hello = "<h1>Hello World</h1>";
-    $name = "Php World";
-    // // dd($_SERVER);
-    // return $hello;
-    // return view('hello', ['name' => $name]);
-    // return view('hello', compact('name'));
-    return view('hello')->with('name', $name);
+Route::get('/test', function () {
+    // $brands = \DB::table('brands')->get();
+    // foreach($brands as $brand){
+    //     dump($brand);
+    // }
+
+    // $brand = \DB::table('brands')->where('id', '=', 1)->first();
+    // $brand = \DB::table('brands')->where('id', 1)->first();
+    // $brand = \DB::table('brands')->find(1);
+    // dump($brand);
+
+    // $names = \DB::table('users')->pluck('name');
+    // foreach($names as $name){
+    //     dump($name);
+    // }
+
+    // $names = \DB::table('users')->pluck('id', 'name');
+    // foreach($names as $name => $id){
+    //     dump($id, $name);
+    // }
+    $brand = \DB::table('brands')->latest()->first();
+    dump($brand);
+    $brand = \DB::table('brands')->oldest()->first();
+    dump($brand);
+
 });
 
 Route::get('/about', [App\Http\Controllers\AboutController::class, 'index'])->name('about-us');
